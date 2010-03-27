8fecc16a5d5d4c5e141085e3d48573e16da1f1d3
Henk Punt <henkpunt@gmail.com>
2010-03-27 17:45:17 +0100

    bla

--- test.py
+++ test.py
@@ -1,5 +1,99 @@
-import redis
+from redis import *
 
-r = redis.Redis(host='localhost', port=6379, db=0)
-for i in range(100000):
-	r.get('blaat')
+import time
+
+class _Timer:
+    def __enter__(self):
+        self._start = time.time()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self._end = time.time()
+
+    def __str__(self):
+        self._end - self._start
+
+    def sec(self, n):
+        return n / (self._end - self._start)
+    
+def timer():
+    return _Timer()
+
+def test_simple():
+    connection = Connection("127.0.0.1:6379")
+
+    for j in range(10):
+        print repr(connection.get('blaat'))
+        
+def test_ketama():
+    ketama = Ketama()
+
+    ketama.add_server(("10.0.1.1", 11211), 600)
+    ketama.add_server(("10.0.1.2", 11211), 300)
+    ketama.add_server(("10.0.1.3", 11211), 200)
+    ketama.add_server(("10.0.1.4", 11211), 350)
+    ketama.add_server(("10.0.1.5", 11211), 1000)
+    ketama.add_server(("10.0.1.6", 11211), 800)
+    ketama.add_server(("10.0.1.7", 11211), 950)
+    ketama.add_server(("10.0.1.8", 11211), 100)
+
+    ketama.create_continuum()
+    #ketama.print_continuum()
+    def check(key, addr):
+        assert addr == ketama.get_server_addr(ketama.get_server(key))
+        
+    check('12936', '10.0.1.7:11211')
+    check('27804', '10.0.1.5:11211')
+    check('37045', '10.0.1.2:11211')
+    check('50829', '10.0.1.1:11211')
+    check('65422', '10.0.1.6:11211')
+    check('74912', '10.0.1.6:11211')
+    
+def test_mget():
+    ketama = Ketama()
+    ketama.add_server(("127.0.0.1", 6379), 300)
+    ketama.add_server(("127.0.0.1", 6380), 300)
+    
+    ketama.create_continuum()
+
+    connection_manager= ConnectionManager()
+    
+    redis = Redis(ketama, connection_manager)
+    
+    
+#    M = 1000
+#    N = 200
+    M = 10
+    N = 10
+    keys = ['piet%d' % i for i in range(N)]
+
+    for i in range(N):
+        redis.set('piet%d' % i, 'blaat%d' % i)
+        assert(redis.get('piet%d' % i) == 'blaat%d' % i)
+        #print '******', repr(redis.get('piet%d' % i))
+    start = time.time()
+    for i in range(M):
+        print redis.mget(*keys)
+    end = time.time()
+    print (N * M) / (end - start)
+    
+def profile(f = None):
+    from cProfile import Profile
+    prof = Profile()
+    try:
+        prof = prof.runctx("f()", globals(), locals())
+    except SystemExit:
+        pass
+
+    import pstats
+    stats = pstats.Stats(prof)
+    stats.strip_dirs()
+    stats.sort_stats('time')
+    stats.print_stats(20)
+
+if __name__ == '__main__':
+    #test_ketama()
+    #test_simple()
+    #profile(test_mget)
+    test_mget()
+    
